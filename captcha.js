179c7db40a17704f6d31766169d3a09359cd7c5e
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2024-12-19 09:08:58 +0300

    all code

---
+++ captcha.js
@@ -0,0 +1,40 @@
+let captchaResolved = false;
+
+function fetchAndDisplaySequence() {
+    const N = document.getElementById('numberInput').value;
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '';
+
+    let i = 1;
+    function fetchNext() {
+        if (i <= N) {
+            fetch('https://api.prod.jcloudify.com/whoami')
+                .then(response => {
+                    if (N >= 100 && response.status === 403 && !captchaResolved) {
+                        showCaptcha();
+                    } else {
+                        outputDiv.innerHTML += `${i}. Forbidden<br>`;
+                        i++;
+                        setTimeout(fetchNext, 1000);
+                    }
+                });
+        }
+    }
+    fetchNext();
+}
+
+function showCaptcha() {
+    const container = document.getElementById("captcha-container");
+    AwsWafCaptcha.renderCaptcha(container, {
+        apiKey: " ",
+        onSuccess: () => {
+            captchaResolved = true;
+            document.getElementById("captcha-container").innerHTML = '';
+            fetchAndDisplaySequence();
+        },
+        onError: (error) => {
+            const errorContainer = document.getElementById("error-container");
+            errorContainer.textContent = error.message;
+        },
+    });
+}
